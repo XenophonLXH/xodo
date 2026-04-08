ca941478083410b2776ba7d440184a2eb86dcab6
Xenophon <lourenzo321@gmail.com>
2026-04-08 20:59:45 +0200

    {IMP} Added the ability to store databases in XDG_DATA_HOME, defaults to user's home if not found. Added ability to manage multiple lists

--- main.go
+++ main.go
@@ -8,11 +8,42 @@ import (
 	tea "charm.land/bubbletea/v2"
 )
 
+func getDatabaseDir(dbname string) string {
+	base := os.Getenv("XDG_DATA_HOME")
+	if base == "" {
+		base = getUserHome()
+	}
+	xodopath := base + "/xodo/databases/"
+
+	if err := os.MkdirAll(xodopath, 0755); err != nil {
+		log.Fatalf("Could not find or create database directory: %v", err)
+	}
+
+	return xodopath + dbname
+}
+
+func getUserHome() string {
+	if homedir, err := os.UserHomeDir(); err != nil {
+		log.Fatalf("Could not determine user's home directory %v", err)
+	} else {
+		return homedir
+	}
+	return ""
+}
+
 func main() {
+
+	var dbname = "xodo"
+	if len(os.Args) > 1 {
+		dbname = os.Args[1]
+	}
+	dbpath := getDatabaseDir(dbname)
+	fmt.Println("Using dbpath: ", dbpath)
+
 	// Get connected to the database
 	s :=  new(Store)
 
-	if err := s.Init(); err != nil {
+	if err := s.Init(dbpath); err != nil {
 		fmt.Println("Could not initiate store:", err)
 		os.Exit(1)
 	}
